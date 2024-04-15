29fd174fa1025bfb35d7b3dc5db1faf61657407b
bartoszjarzynski <bartjar978@student.polsl.pl>
2024-04-15 19:51:42 +0200

    Errors have been resolved

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.set('view engine', 'ejs');
-app.use(express.static("public"));
 app.use('/public/', express.static('./public'));
+app.use(express.static("public"));
 
 app.get("/", (req,res) => {
     res.render("login");
