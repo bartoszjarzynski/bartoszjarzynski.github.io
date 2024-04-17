fd46a71ea7eae4d7a593d8942caa1ef6150e11a0
bartoszjarzynski <bartjar978@student.polsl.pl>
2024-04-17 18:57:53 +0200

    Home page: added new windows

--- public/script.js
+++ public/script.js
@@ -1,120 +1,134 @@
-// Get the canvas element
-var ctx = document.getElementById("pieChart").getContext("2d");
-
-// Define data
-var data = {
-  labels: ["Money Deposit", "Money Spent"],
-  datasets: [
-    {
-      data: [0, 0], // Updated initial data for money deposit and money spent
-      backgroundColor: ["#36A2EB", "#FF6384"], // Colors for each segment
-    },
-  ],
-};
-
-// Configure options
-var options = {
-  responsive: false,
-  maintainAspectRatio: false,
-};
-
-// Create pie chart
-var myPieChart = new Chart(ctx, {
-  type: "pie",
-  data: data,
-  options: options,
+// Moving to the GitHub page from the logo on the home page
+document.addEventListener("DOMContentLoaded", function() {
+  var githubLink = document.getElementById("github-link");
+  githubLink.addEventListener("click", function() {
+    window.open("https://github.com/bartoszjarzynski/finance_monitoring", "_blank");
+  });
 });
 
-// Initialize spending limit
-var spendingsLimit = 1000;
 
-// Deposit and spendings alert creation
-function showAmountPrompt(type) {
-  var action =
-    type === "deposit"
-      ? "deposit"
-      : type === "spendings"
-      ? "spendings"
-      : "limit";
-  var amount = prompt("Enter the amount for " + action + ":");
 
-  if (amount !== null && amount !== "") {
-    var parsedAmount = parseInt(amount); // Parse the amount to integer
+
+
+
+
+// Get the canvas element
+// var ctx = document.getElementById("pieChart").getContext("2d");
+
+// // Define data
+// var data = {
+//   labels: ["Money Deposit", "Money Spent"],
+//   datasets: [
+//     {
+//       data: [0, 0], // Updated initial data for money deposit and money spent
+//       backgroundColor: ["#36A2EB", "#FF6384"], // Colors for each segment
+//     },
+//   ],
+// };
+
+// // Configure options
+// var options = {
+//   responsive: false,
+//   maintainAspectRatio: false,
+// };
+
+// // Create pie chart
+// var myPieChart = new Chart(ctx, {
+//   type: "pie",
+//   data: data,
+//   options: options,
+// });
+
+// // Initialize spending limit
+// var spendingsLimit = 1000;
+
+// // Deposit and spendings alert creation
+// function showAmountPrompt(type) {
+//   var action =
+//     type === "deposit"
+//       ? "deposit"
+//       : type === "spendings"
+//       ? "spendings"
+//       : "limit";
+//   var amount = prompt("Enter the amount for " + action + ":");
+
+//   if (amount !== null && amount !== "") {
+//     var parsedAmount = parseInt(amount); // Parse the amount to integer
     
-    if (type === "deposit") {
-      // Update deposit amount
-      var currentDeposit = parseInt(
-        document.getElementById("depositAmount").textContent
-      );
+//     if (type === "deposit") {
+//       // Update deposit amount
+//       var currentDeposit = parseInt(
+//         document.getElementById("depositAmount").textContent
+//       );
 
-      var newDeposit = currentDeposit + parsedAmount;
-      document.getElementById("depositAmount").textContent = newDeposit + "$";
-      // Update wallet amount
+//       var newDeposit = currentDeposit + parsedAmount;
+//       document.getElementById("depositAmount").textContent = newDeposit + "$";
+//       // Update wallet amount
       
-      var currentWallet = parseInt(
-        document.getElementById("walletAmount").textContent
-      );
+//       var currentWallet = parseInt(
+//         document.getElementById("walletAmount").textContent
+//       );
 
-      var newWallet = currentWallet + parsedAmount;
-      document.getElementById("walletAmount").textContent = newWallet + "$";
+//       var newWallet = currentWallet + parsedAmount;
+//       document.getElementById("walletAmount").textContent = newWallet + "$";
       
-      // Update pie chart's money deposit segment
-      data.datasets[0].data[0] = newWallet;
+//       // Update pie chart's money deposit segment
+//       data.datasets[0].data[0] = newWallet;
       
-      // Update deposit history
-      var depositHistory = document.getElementById("depositHistory");
-      var newDepositEntry = document.createElement("h4");
-      newDepositEntry.textContent = "Deposit: " + parsedAmount + "$";
-      depositHistory.appendChild(newDepositEntry);
-    } else if (type === "spendings") {
-      // Check if spending exceeds the limit
-      if (parsedAmount > spendingsLimit) {
-        alert("Spending amount exceeds the set limit!");
-        return;
-      }
+//       // Update deposit history
+//       var depositHistory = document.getElementById("depositHistory");
+//       var newDepositEntry = document.createElement("h4");
+//       newDepositEntry.textContent = "Deposit: " + parsedAmount + "$";
+//       depositHistory.appendChild(newDepositEntry);
+//     } else if (type === "spendings") {
+//       // Check if spending exceeds the limit
+//       if (parsedAmount > spendingsLimit) {
+//         alert("Spending amount exceeds the set limit!");
+//         return;
+//       }
 
-      // Update spendings amount
-      var currentSpendings = parseInt(
-        document.getElementById("spendingsAmount").textContent
-      );
+//       // Update spendings amount
+//       var currentSpendings = parseInt(
+//         document.getElementById("spendingsAmount").textContent
+//       );
 
-      var newSpendings = currentSpendings + parsedAmount;
-      document.getElementById("spendingsAmount").textContent =
-        newSpendings + "$";
+//       var newSpendings = currentSpendings + parsedAmount;
+//       document.getElementById("spendingsAmount").textContent =
+//         newSpendings + "$";
 
-      // Update wallet amount
-      var currentWallet = parseInt(
-        document.getElementById("walletAmount").textContent
-      );
+//       // Update wallet amount
+//       var currentWallet = parseInt(
+//         document.getElementById("walletAmount").textContent
+//       );
 
-      var newWallet = currentWallet - parsedAmount;
-      document.getElementById("walletAmount").textContent = newWallet + "$";
+//       var newWallet = currentWallet - parsedAmount;
+//       document.getElementById("walletAmount").textContent = newWallet + "$";
       
-      // Update pie chart's money spent segment
-      data.datasets[0].data[1] = newSpendings;
+//       // Update pie chart's money spent segment
+//       data.datasets[0].data[1] = newSpendings;
       
-      // Update spendings history
-      var spendingsHistory = document.getElementById("spendingsHistory");
-      var newSpendingsEntry = document.createElement("h4");
-      newSpendingsEntry.textContent = "Spent: " + parsedAmount + "$";
-      spendingsHistory.appendChild(newSpendingsEntry);
-    } else if (type === "limit") {
+//       // Update spendings history
+//       var spendingsHistory = document.getElementById("spendingsHistory");
+//       var newSpendingsEntry = document.createElement("h4");
+//       newSpendingsEntry.textContent = "Spent: " + parsedAmount + "$";
+//       spendingsHistory.appendChild(newSpendingsEntry);
+//     } else if (type === "limit") {
       
-        // Set spending limit
-      spendingsLimit = parsedAmount;
-      document.getElementById("spendingsLimitValue").textContent =
-        "Set limit is " + spendingsLimit + "$";
-    }
+//         // Set spending limit
+//       spendingsLimit = parsedAmount;
+//       document.getElementById("spendingsLimitValue").textContent =
+//         "Set limit is " + spendingsLimit + "$";
+//     }
 
-    var message = "You have added to " + action + " " + parsedAmount + "$";
-    alert(message);
-    myPieChart.update(); // Update the pie chart
-  }
-}
+//     var message = "You have added to " + action + " " + parsedAmount + "$";
+//     alert(message);
+//     myPieChart.update(); // Update the pie chart
+//   }
+// }
 
 
 
-function signOut() {
-    // Sign out cleaning cookies and entire logic here.
-    window.location.href = '/login';
-}
+// function signOut() {
+//     // Sign out cleaning cookies and entire logic here.
+//     window.location.href = '/login';
+// }
