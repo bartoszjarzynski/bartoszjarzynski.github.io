a6c9f6fdb0c349977fa57db80377f9a39386e81b
bartoszjarzynski <bartjar978@student.polsl.pl>
2024-04-25 23:07:51 +0200

    Left menu has been created

--- public/script.js
+++ public/script.js
@@ -25,7 +25,48 @@ document.addEventListener("DOMContentLoaded", function () {
   span.style.fontWeight = "400";
 });
 
+// Links for left menu clicking
 
+document.addEventListener("DOMContentLoaded", function () {
+  // Event listener for Market button
+  document
+    .querySelector(".market-shortcut")
+    .addEventListener("click", function () {
+      window.open("https://coinmarketcap.com", "_blank"); // Replace with the actual URL
+    });
+
+  // Event listener for News button
+  document
+    .querySelector(".news-shortcut")
+    .addEventListener("click", function () {
+      window.open("https://crypto.news", "_blank"); // Replace with the actual URL
+    });
+
+  // Event listener for Binance button
+  document
+    .querySelector(".binance-shortcut")
+    .addEventListener("click", function () {
+      window.open("https://binance.com", "_blank"); // Replace with the actual URL
+    });
+
+  // Event listener for Twitter button
+  document
+    .querySelector(".twitter-shortcut")
+    .addEventListener("click", function () {
+      window.open("https://x.com", "_blank"); // Replace with the actual URL
+    });
+});
+
+// Buy me a coffee link
+document.addEventListener("DOMContentLoaded", function () {
+  var githubLink = document.getElementById("coffee-link");
+  githubLink.addEventListener("click", function () {
+    window.open(
+      "https://buymeacoffee.com/bjarzynski",
+      "_blank"
+    );
+  });
+});
 
 // Get the canvas element
 // var ctx = document.getElementById("pieChart").getContext("2d");
